99f4c917dd80466ccc76d0de312108581b386785
ninaweber <nina.weber@eon.com>
2026-04-24 14:07:28 +0200

    add quote api

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+async function fetchData() {
+    const url = 'https://motivation-quotes4.p.rapidapi.com/api';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': '174ed2d2d6msha7ca21a3763779ep10d489jsn36ec57bef064',
+            'x-rapidapi-host': 'motivation-quotes4.p.rapidapi.com',
+            'Content-Type': 'application/json'
+        }
+    };
 
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log(result);
+        document.getElementById("quote-id").innerHTML = result.quote;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchData();
